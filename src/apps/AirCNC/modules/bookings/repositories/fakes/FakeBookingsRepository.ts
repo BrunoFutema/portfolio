40bc9022de8991d4e39f6339f86210d07d7f393f
Bruno Futema <bruno.futema@outlook.com>
2020-12-20 01:30:18 -0300

    test(air-cnc): :white_check_mark: Add test of create new booking

---
+++ src/apps/AirCNC/modules/bookings/repositories/fakes/FakeBookingsRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+
+import ICreateBookingDTO from '../../dtos/ICreateBookingDTO';
+import IBookingsRepository from '../IBookingsRepository';
+
+import Booking from '../../infra/typeorm/entities/Booking';
+
+class FakeBookingsRepository implements IBookingsRepository {
+  private bookings: Booking[] = [];
+
+  public async create(data: ICreateBookingDTO): Promise<Booking> {
+    const booking = new Booking();
+
+    Object.assign(booking, { id: uuid() }, data);
+
+    this.bookings.push(booking);
+
+    return booking;
+  }
+
+  public async save(booking: Booking): Promise<Booking> {
+    const findIndex = this.bookings.findIndex(
+      findBooking => findBooking.id === booking.id,
+    );
+
+    this.bookings[findIndex] = booking;
+
+    return booking;
+  }
+}
+
+export default FakeBookingsRepository;
---
+++ src/apps/AirCNC/modules/bookings/services/CreateBookingservice.spec.ts
@@ -0,0 +1,44 @@
+import FakeUsersRepository from '../../users/repositories/fakes/FakeUsersRepository';
+import FakeSpotsRepository from '../../spots/repositories/fakes/FakeSpotsRepository';
+import FakeBookingsRepository from '../repositories/fakes/FakeBookingsRepository';
+import CreateSpotService from '../../spots/services/CreateSpotService';
+import CreateBookingService from './CreateBookingService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeSpotsRepository: FakeSpotsRepository;
+let fakeBookingsRepository: FakeBookingsRepository;
+let createSpotService: CreateSpotService;
+let createBookingService: CreateBookingService;
+
+describe('AirCNC_CreateBookings', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeSpotsRepository = new FakeSpotsRepository();
+    fakeBookingsRepository = new FakeBookingsRepository();
+
+    createSpotService = new CreateSpotService(fakeSpotsRepository);
+    createBookingService = new CreateBookingService(fakeBookingsRepository);
+  });
+
+  it('should be able to create a new AirCNC booking', async () => {
+    const user = await fakeUsersRepository.create({
+      email: 'johndoe@example.com',
+    });
+
+    const spot = await createSpotService.execute({
+      thumbnail: 'thumbnail.png',
+      company: 'Rocketseat',
+      price: 80,
+      techs: 'Node.js, React JS, React Native',
+      user_id: user.id,
+    });
+
+    const booking = await createBookingService.execute({
+      date: new Date(),
+      spot_id: spot.id,
+      user_id: user.id,
+    });
+
+    expect(booking).toHaveProperty('id');
+  });
+});
