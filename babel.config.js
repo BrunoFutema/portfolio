6bb1ed13b82655e4f2579066a7aacf7547dc1d50
Bruno Futema <bruno.futema@outlook.com>
2020-12-20 17:19:16 -0300

    build: :green_heart: Babel build script decorators adjusts

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,19 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
-    ],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript'
   ],
   plugins: [
     ['module-resolver', {
       alias: {
-        '@modules': './src/modules',
+        '@apps': './src/apps',
         '@config': './src/config',
-        '@shared': '.src/shared'
+        '@shared': './src/shared'
       }
-    }]
+    }],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }]
   ],
   ignore: [
     '**/*.spec.ts'
